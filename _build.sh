ee3cd90fe6b4584ca4f9aeca8d6106e6b1355bbf
umarcor <unai.martinezcorral@ehu.eus>
2020-08-06 16:11:35 +0200

    ci: update Dockerfile and workflow
    * Dockerfile:
      * Use image btdi/bookdown
      * Add R package 'reticulate'
    
    * Workflow:
      * Add r-base and R package 'eulerr'

--- _build.sh
+++ _build.sh
@@ -4,6 +4,7 @@ cd $(dirname "$0")
 
 set -ev
 
-Rscript -e "bookdown::render_book('01-index.Rmd', 'bookdown::gitbook')"
-Rscript -e "bookdown::render_book('01-index.Rmd', 'bookdown::pdf_book')"
-Rscript -e "bookdown::render_book('01-index.Rmd', 'bookdown::epub_book')"
+Rscript \
+  -e "bookdown::render_book('01-index.Rmd', 'bookdown::gitbook')" \
+  -e "bookdown::render_book('01-index.Rmd', 'bookdown::pdf_book')" \
+  -e "bookdown::render_book('01-index.Rmd', 'bookdown::epub_book')"
